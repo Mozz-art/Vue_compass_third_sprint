84ed8fabc3f0c0ad6eef3ac173279e672560fbb9
mozzart <Brunocorona@live.com>
2022-03-01 19:56:56 -0300

    Feat: stories created but not modified

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<body onselectstart="return false">
+    <div id="app">
+    <router-view/>
+  </div>
+</body>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App'
+}
+
+</script>
+
+<style lang="scss">
+
+@import './App.scss';
+@import './assets/reset.scss';
+
+</style>
