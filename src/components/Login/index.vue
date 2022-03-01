345f42122b28a9d80c3ad27f73efc673addb67a3
mozzart <Brunocorona@live.com>
2022-03-01 19:34:57 -0300

    Feat: login create

---
+++ src/components/Login/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <section id="conteinerLeft">
+        <div id="mainColumn" >
+            <img draggable="false" id="LogoMidiaQuery" src="@/assets/white-Compass-Login.png" alt="Small logo MidiaQuery">
+            <div class="section1">
+                <h1>Olá,</h1>
+                <p>Para continuar navegando de forma segura, efetue o login na rede.</p>
+            </div>
+            <div id="Login">
+                <div id="LoginEdit">
+                    <p>Login</p>
+                    <InputBox />
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import InputBox from '@/components/input/index.vue';
+export default {
+    name: "Login",
+     components:{
+        InputBox,
+    }
+    
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+@import './index.scss';
+@import '@/assets/MediaQuery.scss';
+
+</style>
