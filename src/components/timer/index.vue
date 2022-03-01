847465f7e1a273296d90340a7d245b85d0441f3e
mozzart <Brunocorona@live.com>
2022-03-01 19:40:50 -0300

    Feat: timer created

---
+++ src/components/timer/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="grid-item" id="seconds"></div>
+  </div>
+</template>
+
+<script>
+import timeout from "@/constants/timeout.js"
+
+export default {
+    name: "Timeout",
+}
+timeout()
+</script>
+
+<style lang="scss" scoped>
+
+@import "./index.scss";
+
+</style>
